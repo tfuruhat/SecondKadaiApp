e15d7bdcabff68a36d488c122b720e4eaebac469
Tsuyoshi Furuhata <tfuruhat@hotmail.com>
2019-02-12 23:08:04 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by ふるふる on 2019/02/12.
+//  Copyright © 2019 Tsuyoshi Furuhata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
